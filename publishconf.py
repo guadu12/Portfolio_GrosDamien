26bfd3f7bf0aeef968c82d6037073ac8af05c732
guadu12 <dgros398@gmail.com>
2026-07-05 12:48:29 +0200

    Update SITEURL to use HTTPS

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = '/eportfolio/bookworm/gros-damien'
+SITEURL = 'https://guadu12.github.io/Portfolio_GrosDamien'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
